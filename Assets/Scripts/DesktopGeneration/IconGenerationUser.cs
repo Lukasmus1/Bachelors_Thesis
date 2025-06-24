67976525428cd7db60da21fc588c9edb79b0658d
Lukáš Píšek <lukin.pisek@email.cz>
2025-06-24 19:19:26 +0200

    fix: special icons generation

--- Assets/Scripts/DesktopGeneration/IconGenerationUser.cs
+++ Assets/Scripts/DesktopGeneration/IconGenerationUser.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using DesktopGeneration.Abstracts;
+using Microsoft.Win32;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,6 +14,15 @@ namespace DesktopGeneration
 {
     public class IconGenerationUser : IconGeneration
     {
+        private static readonly Dictionary<string, Guid> SpecialIcons = new Dictionary<string, Guid>
+        {
+            { "This PC", KnownFolder.ThisPC },
+            { "Network", KnownFolder.Network },
+            { "Recycle Bin", KnownFolder.RecycleBin },
+            { "User Files", KnownFolder.UserFiles },
+            { "Control Panel", KnownFolder.ControlPanel }
+        };
+
         public IconGenerationUser(List<GameObject> desktopIcons) : base(desktopIcons)
         {
         }
@@ -26,6 +37,8 @@ namespace DesktopGeneration
             //Adding files and directories from the user's desktop
             allFiles.AddRange(Directory.GetFiles(userDesktop));
             allFiles.AddRange(Directory.GetDirectories(userDesktop));
+            
+            CheckForSpecialIcons(allFiles);
     
             //Shared desktop for all users
             string commonDesktop = System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonDesktopDirectory);
@@ -41,15 +54,15 @@ namespace DesktopGeneration
             allFiles.Remove(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + @"\desktop.ini");
             allFiles.Remove(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonDesktopDirectory) + @"\desktop.ini");
             
-            for (int iconIndex = 0; iconIndex < allFiles.Count; iconIndex++) 
+            for (int iconIndex = 0; iconIndex < allFiles.Count; iconIndex++)
             {
-                if (Path.GetFileNameWithoutExtension(allFiles[iconIndex]) == "desktop")
-                {
-                    Debug.Log(true);
-                }
+                string iconName;
+                
+                //Checking if the icon is a special icon
+                iconName = allFiles[iconIndex].StartsWith("::") ? GetSpecialIconName(allFiles[iconIndex]) : Path.GetFileNameWithoutExtension(allFiles[iconIndex]);
                 
                 //Setting the text of an icon
-                DesktopIconObjects[iconIndex].GetComponentInChildren<TMP_Text>().text = Path.GetFileNameWithoutExtension(allFiles[iconIndex]);
+                DesktopIconObjects[iconIndex].GetComponentInChildren<TMP_Text>().text = iconName;
                 
                 //Getting the icon image
                 Transform[] images = DesktopIconObjects[iconIndex].GetComponentsInChildren<Transform>();
@@ -66,6 +79,32 @@ namespace DesktopGeneration
                 }
                 DesktopIconObjects[iconIndex].SetActive(true);
             }
+        }
+
+        private static void CheckForSpecialIcons(List<string> allFiles)
+        {
+            const string keyPath = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel";
+            foreach (var icon in SpecialIcons)
+            {
+                Debug.Log((int)Registry.GetValue(keyPath, icon.Value.ToString("B"), 0));
+                if ((int)Registry.GetValue(keyPath, icon.Value.ToString("B"), 0) == 0)
+                {
+                    allFiles.Add("::" + icon.Value.ToString("B"));
+                }
+            }
+        }
+        
+        private static string GetSpecialIconName(string stringGuid)
+        {
+            foreach (KeyValuePair<string, Guid> icon in SpecialIcons)
+            {
+                if (("::" + icon.Value.ToString("B")).Equals(stringGuid))
+                {
+                    return icon.Key;
+                }
+            }
+            
+            return "Unknown Icon";
         }
     }
 }
---
+++ Assets/Scripts/DesktopGeneration/KnownFolder.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DesktopGeneration
+{ 
+    public static class KnownFolder
+    {
+        public static readonly Guid ThisPC = new Guid( "20D04FE0-3AEA-1069-A2D8-08002B30309D" );
+        public static readonly Guid Network = new Guid( "F02C1A0D-BE21-4350-88B0-7367FC96EF3C" );
+        public static readonly Guid RecycleBin = new Guid( "645FF040-5081-101B-9F08-00AA002F954E" );
+        public static readonly Guid UserFiles = new Guid( "59031a47-3f72-44a7-89c5-5595fe6b30ee" );
+        public static readonly Guid ControlPanel = new Guid( "5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0" );
+    }
+}
