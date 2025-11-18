2a1f853b75f88057388f41428cc5a223b3faba88
Lukáš Píšek <lukin.pisek@email.cz>
2025-11-18 13:45:43 +0100

    feat: apps can now be focused

--- Assets/Scripts/Apps/Commons/Commons.cs
+++ Assets/Scripts/Apps/Commons/Commons.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Apps.Commons
 {
-    public class Commons : MonoBehaviour
+    public class Commons : MonoBehaviour, IPointerClickHandler
     {
         public void CloseApp(GameObject appToDestroy)
         {
@@ -12,7 +13,13 @@ namespace Apps.Commons
         public void OpenApp(GameObject appToOpen)
         {
             appToOpen.SetActive(true);
+            appToOpen.transform.SetAsLastSibling();
             AppCommonsModel.Instance.OnAppOpened(appToOpen.name);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            transform.SetAsLastSibling();
         }
     }
 }
