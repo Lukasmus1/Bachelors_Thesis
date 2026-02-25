21b2bf75ef3a28abfd1d22e7c110203385103b33
Lukáš Píšek <lukin.pisek@email.cz>
2026-02-25 19:34:32 +0100

    feat: hidden file finder focus

---
+++ BachelorsThesisHelperApp/Helpers/DesktopHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BachelorsThesisHelperApp.Helpers
+{
+    public static class DesktopHelper
+    {
+
+    }
+}
--- BachelorsThesisHelperApp/MainWindow.xaml.cs
+++ BachelorsThesisHelperApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using BachelorsThesisHelperApp.Helpers;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BachelorsThesisHelperApp;
 
@@ -19,6 +23,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        DispatcherTimer timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromMilliseconds(100);
+
+        timer.Tick += Timer_Tick;
+
+        timer.Start();
+
+        ForceFocus();
     }
     
     private void CloseButton(object sender, RoutedEventArgs e)
@@ -29,5 +42,23 @@ public partial class MainWindow : Window
     private void MinimizeButton(object sender, RoutedEventArgs e)
     {
         WindowState = WindowState.Minimized;
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        
+    }
+
+    private async void ForceFocus()
+    {
+        Type shellType = Type.GetTypeFromProgID("Shell.Application");
+        object shellObject = Activator.CreateInstance(shellType);
+        shellType.InvokeMember("MinimizeAll", BindingFlags.InvokeMethod, null, shellObject, null);
+
+        // Slight delay to finish the animation
+        await Task.Delay(100);
+
+        Topmost = true;
+        Activate();
     }
 }
